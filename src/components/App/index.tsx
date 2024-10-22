dbdb7b362314f87dd9462ad11ef8c374baa70eda
Neotti Luca (EXT Lynx) <luca.neotti@profesia.it>
2024-10-22 14:35:38 +0200

    refactor: move app component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,7 +1,8 @@
 import {
   withAuthenticator,
 } from "@aws-amplify/ui-react";
-import App from "./components/AppDesign";
+import App from "./app";
+import "./index.css";
 
 const withAuthApp = withAuthenticator(App);
 
