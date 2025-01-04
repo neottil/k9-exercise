c626a0c3d8506b1d0abd6ed96d0974f58fa4bbf5
Luca Neotti <neot.luka.89@gmail.com>
2025-01-04 23:09:12 +0100

    feat: add scrollToTop component

--- src/components/App/app.tsx
+++ src/components/App/app.tsx
@@ -1,19 +1,30 @@
+import { PropsWithChildren, useLayoutEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import { WithAuthenticatorProps } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import { ThemeProvider } from "@mui/material/styles";
-import { Outlet } from "react-router-dom";
 
 import "./index.css";
 import theme from "./theme";
 import AppBar from "../AppBar";
 import Footer from "../Footer";
 
+const ScrollToTop = ({ children }: PropsWithChildren) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children
+}
+
 const App = ({ user, signOut }: WithAuthenticatorProps) => (
-  <ThemeProvider theme={theme}>
-    <AppBar user={user} signOut={signOut} />
-    <Outlet context={{ user }} />
-    <Footer />
-  </ThemeProvider>
+  <ScrollToTop>
+    <ThemeProvider theme={theme}>
+      <AppBar user={user} signOut={signOut} />
+      <Outlet context={{ user }} />
+      <Footer />
+    </ThemeProvider>
+  </ScrollToTop>
 );
 
 export default App;
