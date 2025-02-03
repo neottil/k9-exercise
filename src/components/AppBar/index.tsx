36e12a831fce4e229de6c9b8d0e391caa7ce8262
Luca Neotti <neot.luka.89@gmail.com>
2025-02-03 13:39:45 +0100

    feat: home link in title bar

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -1,18 +1,22 @@
 import { WithAuthenticatorProps } from "@aws-amplify/ui-react";
 import AppBar from "@mui/material/AppBar";
 import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+
 import MainMenu from "./menu";
 
 const SiteAppBar = ({ signOut }: WithAuthenticatorProps) => {
   return (
     <AppBar position="sticky">
-      <Toolbar sx={{width: "100%"}}>
+      <Toolbar sx={{ width: "100%" }}>
         <MainMenu />
-        <Typography variant="h5" component="div" sx={{ flexGrow: 1, justifyContent: "center", display: "flex", fontWeight: "bold" }}>
-          K9 Cross Training Exercise Database
-        </Typography>
+        <Link href={"/"} underline="none" color="secondary" sx={{ flexGrow: 1, justifyContent: "center", display: "flex" }}>
+          <Typography variant="h5" component="div" sx={{ fontWeight: "bold" }}>
+            K9 Cross Training Exercise Database
+          </Typography>
+        </Link>
         <Button onClick={signOut} color="secondary">
           Sign out
         </Button>
